6187e16742b881ef5357465735d1e7fa85269ccd
tinashulbert <62221516+tinashulbert@users.noreply.github.com>
2023-05-14 17:15:38 -0400

    Update LoopConstants.swift
    Change to 50% auto bolus

--- Loop/Models/LoopConstants.swift
+++ Loop/Models/LoopConstants.swift
@@ -50,7 +50,7 @@ enum LoopConstants {
     static let retrospectiveCorrectionEnabled = true
     
     // Percentage of recommended dose to apply as bolus when using automatic bolus dosing strategy
-    static let bolusPartialApplicationFactor = 0.4
+    static let bolusPartialApplicationFactor = 0.5
 
     /// The interval over which to aggregate changes in glucose for retrospective correction
     static let retrospectiveCorrectionGroupingInterval = TimeInterval(minutes: 30)
